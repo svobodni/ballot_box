7b6f7ecd28d67e7875fb6611ac5442ff8bac37b3
cocosx <cocosx@gmail.com>
2015-03-08 12:23:38 +0100

    catch error when displaying contact info

--- ballot_box/static/ballot_box.js
+++ ballot_box/static/ballot_box.js
@@ -9,40 +9,44 @@ $(document).ready(function() {
                 var html = "";
                 html += '<img class="pull-left" src="'+data.person.photo_url+'" />';
                 html += '<div class="pull-left">&nbsp;</div><div class="pull-left">'
-                for(var i=0; i<data.person.contacts.length; i++) {
-                    var t = data.person.contacts[i].type;
-                    var v = data.person.contacts[i].value;
-                    if(t == "email") {
-                        html += '<a href="mailto:'+v+'"><i class="fa fa-envelope fa-fw"></i> '+v+'</a>';
-                    } else if(t == "facebook_page") {
-                        html += '<a href="'+v+'"><i class="fa fa-facebook fa-fw"></i> FB stránka</a>';
-                    } else if(t == "facebook_profile") {
-                        html += '<a href="'+v+'"><i class="fa fa-facebook fa-fw"></i> FB profil</a>';
-                    } else if(t == "twitter") {
-                        html += '<a href="https://twitter.com/'+v+'"><i class="fa fa-twitter fa-fw"></i> Twitter</a>';
-                    } else if(t == "web") {
-                        if (!/^https?:\/\//i.test(v)) {
-                            v = 'http://' + v;
+                try {
+                    for(var i=0; i<data.person.contacts.length; i++) {
+                        var t = data.person.contacts[i].type;
+                        var v = data.person.contacts[i].value;
+                        if(t == "email") {
+                            html += '<a href="mailto:'+v+'"><i class="fa fa-envelope fa-fw"></i> '+v+'</a>';
+                        } else if(t == "facebook_page") {
+                            html += '<a href="'+v+'"><i class="fa fa-facebook fa-fw"></i> FB stránka</a>';
+                        } else if(t == "facebook_profile") {
+                            html += '<a href="'+v+'"><i class="fa fa-facebook fa-fw"></i> FB profil</a>';
+                        } else if(t == "twitter") {
+                            html += '<a href="https://twitter.com/'+v+'"><i class="fa fa-twitter fa-fw"></i> Twitter</a>';
+                        } else if(t == "web") {
+                            if (!/^https?:\/\//i.test(v)) {
+                                v = 'http://' + v;
+                            }
+                            html += '<a href="'+v+'"><i class="fa fa-globe fa-fw"></i> Web</a>';
+                        } else if(t == "blog") {
+                            html += '<a href="'+v+'"><i class="fa fa-book fa-fw"></i> Blog</a>';
+                        } else if(t == "google_plus") {
+                            html += '<a href="'+v+'"><i class="fa fa-google-plus fa-fw"></i> Google+</a>';
+                        } else if(t == "linked_in") {
+                            html += '<a href="'+v+'"><i class="fa fa-linkedin fa-fw"></i> LinkedIn</a>';
+                        } else if(t == "phone") {
+                            html += '<i class="fa fa-phone fa-fw"></i> '+v;
                         }
-                        html += '<a href="'+v+'"><i class="fa fa-globe fa-fw"></i> Web</a>';
-                    } else if(t == "blog") {
-                        html += '<a href="'+v+'"><i class="fa fa-book fa-fw"></i> Blog</a>';
-                    } else if(t == "google_plus") {
-                        html += '<a href="'+v+'"><i class="fa fa-google-plus fa-fw"></i> Google+</a>';
-                    } else if(t == "linked_in") {
-                        html += '<a href="'+v+'"><i class="fa fa-linkedin fa-fw"></i> LinkedIn</a>';
-                    } else if(t == "phone") {
-                        html += '<i class="fa fa-phone fa-fw"></i> '+v;
+                        html += '<br/>'
                     }
-                    html += '<br/>'
-                }
-                if(data.person.cv_url) {
-                    html += '<a href="'+data.person.cv_url+'"><i class="fa fa-file-text-o fa-fw"></i> CV</a><br/>';   
+                    if(data.person.cv_url) {
+                        html += '<a href="'+data.person.cv_url+'"><i class="fa fa-file-text-o fa-fw"></i> CV</a><br/>';   
+                    }
+                } catch (e) {
+                    console.log("Chyba pri zobrazovani kontaktu.");
+                    console.log(e);
+                    console.log(data);
                 }
                 html += "</div>"
-                console.log(html);
                 $this.html(html);
-                console.log(data);
 
             },
             error: function() {
